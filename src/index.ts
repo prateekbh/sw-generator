18c6cb64f6eb28c8f4b914f0f68575ddecbd0b3d
Prateek Bhatnagar <prateek89born@gmail.com>
2018-10-11 15:29:44 -0700

    First commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import {ampAssetsCaching} from './modules/amp-caching/index';
+
+// Add the import script for workbox
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/__WORKBOX__VERSION__/workbox-sw.js');
+
+ampAssetsCaching();
