9fc47a0629dd45ce265ac1ea6c9b467bc8fcd880
Prateek Bhatnagar <prateek89born@gmail.com>
2018-10-12 16:43:20 -0700

    adding tests

--- test/amp-caching/amp-caching-test.js
+++ test/amp-caching/amp-caching-test.js
@@ -5,8 +5,8 @@ const serve = require('serve');
 
 describe("Amp caching", function () {
   let server, browser, page;
-
   this.timeout(5000);
+
   before(async () => {
     const serveDir = path.join(__dirname, '../');
     server = serve(serveDir, {
@@ -21,10 +21,6 @@ describe("Amp caching", function () {
 
     browser = await puppeteer.launch();
     page = await browser.newPage();
-    page.on('console', msg => {
-      for (let i = 0; i < msg.args().length; ++i)
-        console.log(`${i}: ${msg.args()[i]}`);
-    });
     await page.goto('http://localhost:8080/amp-caching/');
   });
 
@@ -37,7 +33,6 @@ describe("Amp caching", function () {
     await page.evaluate(() => window.__testCleanup());
     await page.evaluate(async () => {
       const registration = await navigator.serviceWorker.register('/amp-caching/sw.js');
-      console.log(registration.installing);
       await window.__waitForSWState(registration, 'activated');
     });
     const swRegCount = await page.evaluate(async () => {
@@ -56,6 +51,9 @@ describe("Amp caching", function () {
   });
 
   describe('Versioned JS', () => {
+    const versionedAmpRuntime = 'https://cdn.ampproject.org/rtv/001525381599226/v0.js';
+    const versionedAmpExtension = 'https://cdn.ampproject.org/rtv/001525381599226/v0.js';
+
     it('should create a cache in cache name', async () => {
       let hasVersionJSInCache = await page.evaluate(async () => {
         return await caches.has('AMP-SW-CACHE');
@@ -64,19 +62,30 @@ describe("Amp caching", function () {
       expect(hasVersionJSInCache).to.be.equal(false);
 
       // A request to a versioned js file should create the cache
-      hasVersionJSInCache = await page.evaluate(async () => {
-        await fetch('https://cdn.ampproject.org/rtv/001525381599226/v0.js');
+      hasVersionJSInCache = await page.evaluate(async (versionedAmpRuntime) => {
+        await fetch(versionedAmpRuntime);
         return await caches.has('AMP-SW-CACHE');
-      });
+      }, versionedAmpRuntime);
       expect(hasVersionJSInCache).to.be.equal(true);
     });
 
     it('should fetch and store the versioned jS', async () => {
-      const cacheResponse = await page.evaluate(async () => {
+      const cacheResponse = await page.evaluate(async (versionedAmpRuntime) => {
         const cache = await caches.open('AMP-SW-CACHE')
-        return await cache.match('https://cdn.ampproject.org/rtv/001525381599226/v0.js');
-      });
+        return await cache.match(versionedAmpRuntime);
+      }, versionedAmpRuntime);
       expect(cacheResponse).to.not.be.null;
+    });
+
+    it('should not fetch versioned js anymore from network', async () => {
+      const DUMMY_RESPONSE = 'dummy response';
+      const fetchResponse = await page.evaluate(async (versionedAmpRuntime, DUMMY_RESPONSE) => {
+        const cache = await caches.open('AMP-SW-CACHE')
+        await cache.put(new Request(versionedAmpRuntime), new Response(DUMMY_RESPONSE));
+        const response = await fetch(versionedAmpRuntime);
+        return await response.text();
+      }, versionedAmpRuntime, DUMMY_RESPONSE);
+      expect(fetchResponse).to.be.equal(DUMMY_RESPONSE);
     });
   });
 });
