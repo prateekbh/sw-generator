01d877258d5feccdbef078b9da6694eb0f1492a2
Prateek Bhatnagar <prateek89born@gmail.com>
2018-10-12 17:30:39 -0700

    make SW generator

--- config/rollup.config.js
+++ config/rollup.config.js
@@ -2,11 +2,9 @@ import replace from 'rollup-plugin-replace';
 import pkg from '../package.json';
 
 export default {
-    input: 'output/index.js',
     output: {
       name: 'AmpServiceWorker',
       format: 'es',
-      file: 'build/index.js',
       sourceMap: true,
     },
     plugins: [
---
+++ index.js
@@ -0,0 +1,24 @@
+const tmp = require('tmp');
+const npmRun = require('npm-run');
+const fs= require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+const tmpobj = tmp.dirSync();
+const tempDir = tmpobj.name;
+
+// Transpile TSC in a temp dir
+npmRun.sync(`tsc -p tsconfig.json --outDir ${tempDir}`);
+
+const filename = path.join(tempDir, 'index.js');
+const outfile = path.join(tempDir, 'build', 'index.js');
+
+// rollup the files in the tempdir
+npmRun.sync(`rollup ${filename} --config config/rollup.config.js --file ${outfile}`);
+
+// return the generated SW
+const swText = fs.readFileSync(outfile, {
+    encoding: 'UTF-8',
+});
+
+return swText;
