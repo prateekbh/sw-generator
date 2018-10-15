a9bc99c78d9e14131d7da9b1f0758e60f65658b2
Prateek Bhatnagar <prateek89born@gmail.com>
2018-10-15 11:58:52 -0700

    using the node module approach

--- index.js
+++ index.js
@@ -1,21 +1,36 @@
-const tmp = require('tmp');
-const npmRun = require('npm-run');
-const fs= require('fs');
-const path = require('path');
+import {dir} from 'tmp';
+import npmRun from 'npm-run';
+import {join} from 'path';
+import {rollup} from 'rollup';
+import {promisify} from 'util';
+import replace from 'rollup-plugin-replace';
+import {devDependencies} from './package.json'
 
-const tmpobj = tmp.dirSync();
-const tempDir = tmpobj.name;
+export async function buildSW() {
+  const createTempDir = promisify(dir);
+  const tempDir = await createTempDir({});
 
-// Transpile TSC in a temp dir
-npmRun.sync(`tsc -p tsconfig.json --outDir ${tempDir}`);
+  // Transpile TSC in a temp dir
+  npmRun.sync(`tsc -p ./src/tsconfig.json --outDir ${tempDir}`);
 
-const filename = path.join(tempDir, 'index.js');
-const outfile = path.join(tempDir, 'build', 'index.js');
+  const filename = join(tempDir, 'index.js');
 
-// rollup the files in the tempdir
-npmRun.sync(`rollup ${filename} --config config/rollup.config.js --file ${outfile}`);
+  debugger;
+  // rollup the files in the tempdir
+  const bundle = await rollup({
+    input: filename,
+    plugins: [
+      replace({
+          __WORKBOX__VERSION__: devDependencies['workbox-sw']
+      })
+    ]
+  });
 
-// return the generated SW
-return fs.readFileSync(outfile, {
-    encoding: 'UTF-8',
-});
+  const {code} = await bundle.generate({
+    name: 'AmpServiceWorker',
+    format: 'es',
+    sourceMap: true,
+  });
+
+  return code;
+}
