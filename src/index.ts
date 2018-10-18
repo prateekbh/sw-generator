87e478be94e02e0ca51cc6c4e576882f8e704ec4
Prateek Bhatnagar <prateek89born@gmail.com>
2018-10-18 11:55:01 -0700

    removing workbox core

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
 import { ampAssetsCaching } from './modules/amp-caching/index';
-import 'workbox-core';
-
-// Add the import script for workbox
-importScripts(
-  'https://storage.googleapis.com/workbox-cdn/releases/__WORKBOX__VERSION__/workbox-sw.js',
-);
 
 ampAssetsCaching();
