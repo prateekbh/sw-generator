6f54cc80274cc16529a6948d153f0f032224fd6d
Prateek Bhatnagar <prateek89born@gmail.com>
2018-10-29 14:48:15 -0700

    fixing tests

--- test/asset-caching/asset-caching-test.js
+++ test/asset-caching/asset-caching-test.js
@@ -14,7 +14,7 @@ const unlink = promisify(fs.unlink);
 const serviceWorkerPath = join('test', 'asset-caching-sw.js');
 const cacheName = 'AMP-ASSET-CACHE';
 
-describe('Document Caching Module', function() {
+describe('Asset Caching Module', function() {
   const driver = global.__AMPSW.driver;
   const regexp = /^http:\/\/localhost:6881\/test\/fixtures\//;
 
